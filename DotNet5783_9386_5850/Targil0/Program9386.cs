ec2d60c264f2d79e7cfbd9b82c3c3c3cd53ce8d2
Omer-DN <Omerdayan94@gmail.com>
2022-11-02 14:15:25 +0200

    add progras9386

---
+++ DotNet5783_9386_5850/Targil0/Program9386.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+namespace Targil0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcom9386();
+            Welcome5850();
+            Console.ReadKey();
+        }
+
+        private static void Welcom9386()
+        {
+            Console.WriteLine("Enter your name");
+            string userName = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", userName);
+        }
+        static partial void Welcome5850();
+    }
+}
