a25fe0d004cac4eab077dab717edce6f7da2fe8d
Omer-DN <Omerdayan94@gmail.com>
2022-11-03 18:09:59 +0200

    add inti

--- DotNet5783_9386_5850/DalList/Dal/DataSource.cs
+++ DotNet5783_9386_5850/DalList/Dal/DataSource.cs
@@ -1,8 +1,14 @@
 ﻿
 
+using DO;
+
 namespace Dal;
 
 internal class DataSource
 {
+     readonly Random rand = new Random();
+    Product[] arrayOfproducts = new Product[50];
+    Order[] arrayOforders = new Order[100];
+    OrderItem[] arrayOrderItems = new OrderItem[200];       
 
 }
