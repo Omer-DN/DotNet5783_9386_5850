aeb7d3ddd4a7a4cd464bb20b9e5de1f187ae69f1
elad3434 <elad3434@gmail.com>
2022-11-03 17:38:55 +0200

    i add Order class

--- DotNet5783_9386_5850/DalFacade/DO/Order.cs
+++ DotNet5783_9386_5850/DalFacade/DO/Order.cs
@@ -1,4 +1,7 @@
 ﻿
+using static DO.Enums;
+using System.Diagnostics;
+
 namespace DO;
 /// <summary>
 /// Structure for order data
@@ -6,4 +9,22 @@ namespace DO;
 public struct Order
 {
     public int Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string DeliveryAdress { get; set; }
+    public DateTime OrderCreationDate { get; set; }
+    public DateTime ShippingDate { get; set; } 
+    public DateTime DeliveryDate  { get; set; }
+
+    public override string ToString()
+    {
+        return
+        $@"Order ID = {Id}
+        Name = {Name} 
+        Email - {Email} 
+        Delivery Adress: {DeliveryAdress}
+        Order Creation: {OrderCreationDate}
+        Shipping Date: {ShippingDate}
+        Delivery Date: {DeliveryDate}";
+    }
 }
