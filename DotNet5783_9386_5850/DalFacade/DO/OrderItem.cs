28bd2bdd6e00942f368cf93f21c0f7f36d469e52
elad3434 <elad3434@gmail.com>
2022-11-03 18:56:32 +0200

    ...

--- DotNet5783_9386_5850/DalFacade/DO/OrderItem.cs
+++ DotNet5783_9386_5850/DalFacade/DO/OrderItem.cs
@@ -1,4 +1,7 @@
 ﻿
+using static DO.Enums;
+using System.Xml.Linq;
+
 namespace DO;
 
 public struct OrderItem
@@ -7,5 +10,14 @@ public struct OrderItem
     public int OrderID { get; set; }
     public double Price { get; set; }
     public int Amount { get; set; }
+
+    public override string ToString()
+    {
+        return
+        $@"Product ID = {ProductID}
+        OrderID - {OrderID} 
+        Price: {Price}
+        Amount: {Amount}";
+    }
 
 }
