8415483ef50e1fd00c3dda98c080b0f4a9f3e504
elad3434 <elad3434@gmail.com>
2022-11-03 18:27:22 +0200

    ...

--- DotNet5783_9386_5850/DalList/Dal/DataSource.cs
+++ DotNet5783_9386_5850/DalList/Dal/DataSource.cs
@@ -1,14 +1,33 @@
 ﻿
 
 using DO;
+using System.ComponentModel;
+using static DO.Enums;
 
 namespace Dal;
 
 internal class DataSource
 {
-     readonly Random rand = new Random();
-    Product[] arrayOfproducts = new Product[50];
+    readonly Random rand = new Random();
+    static Product[] arrayOfproducts = new Product[50];
     Order[] arrayOforders = new Order[100];
     OrderItem[] arrayOrderItems = new OrderItem[200];       
+
+    public static void AddProduct(int id, string name, double price, Category category, int instock)
+    {
+        Product newProduct = new Product();
+        newProduct.Id = id;
+        newProduct.Name = name;
+        newProduct.Price = price;
+        newProduct.Category = category;
+        newProduct.InStock = instock;
+        for (int i=0; i<50; i++)
+        {
+            if (arrayOfproducts[i] is null)
+            {
+
+            }
+        }
+    }
 
 }
