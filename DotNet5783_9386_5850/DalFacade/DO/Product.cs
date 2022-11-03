9929f40042c37a584c3cc5b0b87c057f23d2bdc8
Omer-DN <Omerdayan94@gmail.com>
2022-11-03 10:17:13 +0200

    add ToString method

--- DotNet5783_9386_5850/DalFacade/DO/Product.cs
+++ DotNet5783_9386_5850/DalFacade/DO/Product.cs
@@ -1,7 +1,15 @@
 ﻿
+using System.Diagnostics;
+using System.Xml.Linq;
+
 namespace DO;
 
 public struct Product
 {
     public int Id { get; set; }
+    public override string ToString() => $@"
+Product ID={ID}: {Name}, 
+category - {Category}
+    	Price: {Price}
+    	Amount in stock: {InStock}
 }
