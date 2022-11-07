724493cdb992bb990e64504fc5aadd2a6c72c333
Omer-DN <Omerdayan94@gmail.com>
2022-11-07 17:15:53 +0200

    try creat price### in parameter datasource file

--- DotNet5783_9386_5850/DalFacade/DO/Enums.cs
+++ DotNet5783_9386_5850/DalFacade/DO/Enums.cs
@@ -2,7 +2,7 @@
 namespace DO;
 
 public struct Enums
-{
+{ 
     public enum CostumerNames {Shimon,David,Reuben,Yakov,Shlomo,Moshe,Yuval,Elad,Omer,Yonatan}
     public enum CostumerAdress { Shimon74, David13, Reuben2, Yakov8, Shlomo65, Moshe27, Yuval44, Elad35, Omer24, Yonatan79 }
 
