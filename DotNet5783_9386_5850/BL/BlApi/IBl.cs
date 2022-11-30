9d9aeb57854aa589c6cdc410bc97153620cb0128
Omer-DN <Omerdayan94@gmail.com>
2022-11-30 10:04:17 +0200

    add IBl interface

---
+++ DotNet5783_9386_5850/BL/BlApi/IBl.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlApi
+{
+    public interface IBl
+    {
+        public IProduct product { get; }
+
+        public IOrder order { get; }
+
+        public ICart cart { get;}
+    }
+}
