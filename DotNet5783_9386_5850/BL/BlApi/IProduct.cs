9c9551c86dc6a743c15457cd47f8c2d3b3309237
Omer-DN <Omerdayan94@gmail.com>
2022-11-30 09:21:19 +0200

    I tried to write the actions for Product

--- DotNet5783_9386_5850/BL/BlApi/IProduct.cs
+++ DotNet5783_9386_5850/BL/BlApi/IProduct.cs
@@ -8,15 +8,43 @@ using BL.BO;
 using BO;
 using Dal;
 using DalApi;
+using DO;
 
 namespace BlApi
 {
     public interface IProduct
     {
+        /// Request a list of products from the data layer
+        /// Build a list of products of the ProductForList type (logical entity) based on the data
+        /// Product list request
+        /// Return the constructed list
         public IEnumerable<BoProduct?> GetProducts()
         {
             return new List<BoProduct?>();
 
+        }
+
+        /// You will receive a product ID
+        /// If the ID is a positive number - you will make an attempt to request a product from the data layer
+        /// Build a Product object (logical entity) based on the received data and calculate missing information
+        /// <param name="id"></param>
+        /// will return a constructed Product object
+        /// throw its own appropriate exception Product request failed -
+        /// (product does not exist in data layer - catch exception)
+        public Dal.DalProduct product (int id)
+        {
+            if (id > 0)
+            {
+                try
+                {
+                    product.Get(id);
+
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("not valid id");
+                }
+            }
         }
 
     }
