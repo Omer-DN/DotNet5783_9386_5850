db0e1d35ed35ca94d64e4df07ec4641e37c726fb
elad3434 <elad3434@gmail.com>
2022-12-07 14:10:07 +0200

    add Bl class thet implement the IBI interface

---
+++ DotNet5783_9386_5850/BL/BlImplementation/Bl.cs
@@ -0,0 +1,18 @@
+﻿using BlApi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlImplementation
+{
+    sealed public class Bl : IBl
+    {
+        public IBoCart BoCart => new BoCart();
+        public IBoOrder BoOrder => new BoOrder();
+        public IBoProduct BoProduct => new BoProduct();
+
+    }
+}
