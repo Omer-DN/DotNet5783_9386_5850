a14db44c819bbfe18822d6fe55412d0550eefdda
Omer-DN <Omerdayan94@gmail.com>
2022-12-13 15:10:45 +0200

    add line

--- DotNet5783_9386_5850/BL/BlApi/IBOCart .cs
+++ DotNet5783_9386_5850/BL/BlApi/IBOCart .cs
@@ -3,10 +3,10 @@
 namespace BlApi
 {
     public interface IBoCart
-    {
+    { 
         public IEnumerable<BO.BoCart> GetBoCarts();
         public BO.BoCart AddItem(BO.BoCart cart, int id);
-        public BO.BoOrder UpdateItem(BO.BoCart cart, int amount, int id);
-        public void OrderConfir(BO.BoCart cart, string name, string email, string adress);
+        public BO.BoCart UpdateItem(BO.BoCart cart, int amount, int id);
+        public void OrderConfirmation(BO.BoCart cart, string name, string email, string adress);
     }
 }
