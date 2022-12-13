a79aa3698a2454bc387e551b8fbc02194a40c8ed
Omer-DN <Omerdayan94@gmail.com>
2022-12-13 12:01:43 +0200

    .

--- DotNet5783_9386_5850/BL/BlApi/IBOCart .cs
+++ DotNet5783_9386_5850/BL/BlApi/IBOCart .cs
@@ -7,8 +7,6 @@ namespace BlApi
         public IEnumerable<BO.BoCart> GetBoCarts();
         public BO.BoCart AddItem(BO.BoCart cart, int id);
         public BO.BoOrder UpdateItem(BO.BoCart cart, int amount, int id);
-        public BO.BoCart OrderConfir(BO.BoCart cart, string name, string email, string adress);
-
-
+        public void OrderConfir(BO.BoCart cart, string name, string email, string adress);
     }
 }
