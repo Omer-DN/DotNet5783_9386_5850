23f5c370f843f384a05630f5d6b0a53553c2f635
Omer-DN <Omerdayan94@gmail.com>
2022-12-14 09:32:41 +0200

    add '?' Nullable Value

--- DotNet5783_9386_5850/DalFacade/DO/Order.cs
+++ DotNet5783_9386_5850/DalFacade/DO/Order.cs
@@ -8,9 +8,9 @@ namespace DO;
 public struct Order
 {
     public int ID { get; set; }
-    public string CostumerName { get; set; }
-    public string CostumerEmail { get; set; }
-    public string CostumerAdress { get; set; }
+    public string? CostumerName { get; set; }
+    public string? CostumerEmail { get; set; }
+    public string? CostumerAdress { get; set; }
     public DateTime? OrderDate { get; set; }
     public DateTime? ShipDate { get; set; } 
     public DateTime? DeliveryDate  { get; set; }
--- DotNet5783_9386_5850/DalFacade/DO/Product.cs
+++ DotNet5783_9386_5850/DalFacade/DO/Product.cs
@@ -6,7 +6,7 @@ namespace DO;
 public struct Product
 {
     public int ID { get; set; }
-    public string Name { get; set; }
+    public string? Name { get; set; }
     public double Price { get; set; }
     public Category Category { get; set; }
     public int InStock { get; set; }
