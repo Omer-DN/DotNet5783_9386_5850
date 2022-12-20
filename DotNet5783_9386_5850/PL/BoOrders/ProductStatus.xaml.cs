9ea80703ebaaf636a07223edc3a0b09786f8bffe
Omer-DN <Omerdayan94@gmail.com>
2022-12-20 20:02:15 +0200

    step 3-> grin inside drid

---
+++ DotNet5783_9386_5850/PL/BoOrders/ProductStatus.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PL.BoOrders
+{
+    /// <summary>
+    /// Interaction logic for ProductStatus.xaml
+    /// </summary>
+    public partial class ProductStatus : Window
+    {
+        public ProductStatus()
+        {
+            InitializeComponent();
+        }
+    }
+}
