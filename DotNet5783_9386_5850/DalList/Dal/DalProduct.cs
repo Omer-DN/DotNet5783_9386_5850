7041a6c92b6a1e84cac74d2dedfa627350dbe72e
Omer-DN <Omerdayan94@gmail.com>
2023-01-16 20:48:19 +0200

    do in LINQ

--- DotNet5783_9386_5850/DalList/Dal/DalProduct.cs
+++ DotNet5783_9386_5850/DalList/Dal/DalProduct.cs
@@ -11,28 +11,39 @@ public class DalProduct:IProduct
 {
     public int Add(Product product)
     {
-        if(!(listOfProducts== null))
-            foreach (Product i in listOfProducts)
+        if (listOfProducts != null)
+        {
+            if (listOfProducts.Any(i => i.ID == product.ID))
             {
-               if (i.ID == product.ID)
-                    throw new IdAlreadyExist("This product already exists in the system");
+                throw new IdAlreadyExist("This product already exists in the system");
             }
-        //product.ID = DataSource.getlastProductId(); //stage 1
+        }
+        //if(!(listOfProducts== null))
+        //    foreach (Product i in listOfProducts)
+        //    {
+        //       if (i.ID == product.ID)
+        //            throw new IdAlreadyExist("This product already exists in the system");
+        //    }
         listOfProducts.Add(product);
         return product.ID;
     }
 
     public void Delete(int id)
+
     {
-        foreach (Product i in listOfProducts)
-        {
-            if (i.ID == id)
-            {
-                listOfProducts.Remove(i);
-                return;
-            }
-        }
-        throw new IdNotFound("This product does not exist in the system");
+        if (!listOfProducts.Any(i => i.ID == id))
+            throw new IdNotFound("This product does not exist in the system");
+        listOfProducts.RemoveAll(i => i.ID == id);
+
+        //foreach (Product i in listOfProducts)
+        //{
+        //    if (i.ID == id)
+        //    {
+        //        listOfProducts.Remove(i);
+        //        return;
+        //    }
+        //}
+        //throw new IdNotFound("This product does not exist in the system");
     }
 
     public void Update(Product product)
@@ -47,6 +58,13 @@ public class DalProduct:IProduct
     }
 
     public Product Get(int id)
+    //{
+    //    var find_product = listOfProducts.FirstOrDefault(i => i.ID == id);
+    //    if (find_product == null)
+    //        throw new Exception("This product does not exist in the system");
+    //    else
+    //        return find_product;
+    //}
     {
         foreach (Product i in listOfProducts)
             if (i.ID == id)
@@ -55,26 +73,43 @@ public class DalProduct:IProduct
     }
 
     public Product GetCond(int id, Func<Product, bool>? condition)
+
     {
-        foreach (Product i in listOfProducts)
-            if (i.ID == id && condition!(i))
-                return i;
-        throw new Exception("This product does not exist in the system");
+        try
+        {
+            return listOfProducts.First(i => i.ID == id && condition!(i));
+        }
+        catch (InvalidOperationException)
+        {
+            throw new Exception("This product does not exist in the system");
+        }
     }
+    //{
+    //    foreach (Product i in listOfProducts)
+    //        if (i.ID == id && condition!(i))
+    //            return i;
+    //    throw new Exception("This product does not exist in the system");
+    //}
 
     // return a List of current products in the store
     public IEnumerable<Product> GetList(Func<Product, bool>? condition )
-    {    
-        List<Product> products = new List<Product>();
-        if (condition == null) 
-        {
-            foreach (Product i in listOfProducts)
-                products.Add(i);
-        }
-        else
-            products = listOfProducts.FindAll(x => condition(x));
-           
-       return products;
+
+    {
+        return (condition == null) ?
+        listOfProducts.ToList() :
+        listOfProducts.Where(x => condition(x)).ToList();
     }
+    //{    
+    //    List<Product> products = new List<Product>();
+    //    if (condition == null) 
+    //    {
+    //        foreach (Product i in listOfProducts)
+    //            products.Add(i);
+    //    }
+    //    else
+    //        products = listOfProducts.FindAll(x => condition(x));
+           
+    //   return products;
+    //}
 
 }
