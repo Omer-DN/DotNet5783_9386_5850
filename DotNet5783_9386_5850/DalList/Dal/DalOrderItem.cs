c1b0e48763197f9f5c60d9ea7e1c12858ac73580
Omer-DN <Omerdayan94@gmail.com>
2023-01-18 19:14:41 +0200

    using LINQobject

--- DotNet5783_9386_5850/DalList/Dal/DalOrderItem.cs
+++ DotNet5783_9386_5850/DalList/Dal/DalOrderItem.cs
@@ -9,27 +9,32 @@ public class DalOrderItem:IOrderItem
 {
     public int Add(OrderItem Orderitem)
     {
-        var duplicateItem = listOfOrderItems.GroupBy(x => x.ID)
-                                            .Where(g => g.Count() > 1)
-                                            .SelectMany(g => g)
-                                            .FirstOrDefault(x => x.ID == Orderitem.ID);
-        if (duplicateItem != null)
+        var orderItemIds = listOfOrderItems
+            .GroupBy(x => x.ID)
+            .Select(g => g.First())
+            .Select(i => i.ID)
+            .OrderBy(id => id)
+            .ToList();
+
+        var lastOrderItemId = orderItemIds.Max() + 1;
+
+        var newOrderItem = new OrderItem
         {
-            throw new Exception("This order item already exists in the order items list");
-        }
+            ID = lastOrderItemId,
+            // set other properties here
+        };
 
-        Orderitem.ID = getlastOrderItemId();
-        listOfOrderItems.Add(Orderitem);
-        return Orderitem.ID;
+        listOfOrderItems.Add(newOrderItem);
 
+        return newOrderItem.ID;
 
     }
     //{
     //    foreach (OrderItem i in listOfOrderItems)
     //        if (i.ID == Orderitem.ID)
     //            throw new Exception("This order item already exists in the order items list");
-    //    Orderitem.ID = getlastOrderItemId();
-    //    listOfOrderItems.Add(Orderitem);
+    //Orderitem.ID = getlastOrderItemId();
+    //listOfOrderItems.Add(Orderitem);
     //    return Orderitem.ID;
     //}
 
@@ -53,56 +58,56 @@ public class DalOrderItem:IOrderItem
     //}
 
     public void Update(OrderItem Orderitem)
-    //{
-    //    var orderItem = listOfOrderItems.FirstOrDefault(x => x.ID == Orderitem.ID);
-    //    if (orderItem == null)
-    //        throw new Exception("This order item does not exist in the system");
-    //    int index = listOfOrderItems.IndexOf(orderItem);
-    //    listOfOrderItems[index] = Orderitem;
-
-    //}
     {
-        foreach (OrderItem i in listOfOrderItems)
-            if (i.ID == Orderitem.ID)
-            {
-                var index = listOfOrderItems.FindIndex(i => i.ID == Orderitem.ID);
-    listOfOrderItems[index] = Orderitem;
-            }
+        var orderItemIndex = listOfOrderItems.FindIndex(i => i.ID == Orderitem.ID);
+        if (orderItemIndex != -1)
+        {
+            listOfOrderItems[orderItemIndex] = Orderitem;
+            return;
+        }
         throw new Exception("This order item does not exist in the system");
+
     }
+    //{
+    //    foreach (OrderItem i in listOfOrderItems)
+    //        if (i.ID == Orderitem.ID)
+    //        {
+    //            var index = listOfOrderItems.FindIndex(i => i.ID == Orderitem.ID);
+    //listOfOrderItems[index] = Orderitem;
+    //        }
+    //    throw new Exception("This order item does not exist in the system");
+    //}
 
     public OrderItem Get(int id)
-    //{
-    //    var orderItem = listOfOrderItems.FirstOrDefault(x => x.ID == id);
-    //    if (orderItem == null)
-    //        throw new Exception("This order item does not exist in the system");
-    //    return orderItem;
-
-    //}
     {
-        foreach (OrderItem i in listOfOrderItems)
-            if (i.ID == id)
-                return i;
-        throw new Exception("This order item does not exist in the system");
-}
+        var orderItem = listOfOrderItems.FirstOrDefault(i => i.ID == id);
+        if (orderItem != null)
+            return orderItem;
+        else
+            throw new Exception("This order item does not exist in the system");
+    }
+    //{
+    //    foreach (OrderItem i in listOfOrderItems)
+    //        if (i.ID == id)
+    //            return i;
+    //    throw new Exception("This order item does not exist in the system");
+    //}
 
     public OrderItem GetCond(int id, Func<OrderItem, bool>? condition)
-    //{
-    //    var orderItem = (from i in listOfOrderItems
-    //                     let match = i.ID == id && condition(i)
-    //                     where match
-    //                     select i).FirstOrDefault();
-    //    if (orderItem == null)
-    //        throw new Exception("This order item does not exist in the system");
-    //    else
-    //        return orderItem;
-    //}
     {
-        foreach (OrderItem i in listOfOrderItems)
-            if (i.ID == id && condition(i))
-                return i;
-        throw new Exception("This order item does not exist in the system");
+        var orderItem = listOfOrderItems.Where(i => i.ID == id).FirstOrDefault(condition);
+        if (orderItem != null)
+            return orderItem;
+        else
+            throw new Exception("This order item does not exist in the system");
+
     }
+    //{
+    //    foreach (OrderItem i in listOfOrderItems)
+    //        if (i.ID == id && condition(i))
+    //            return i;
+    //    throw new Exception("This order item does not exist in the system");
+    //}
 
     // return a List of current order items in all the orders of the store
     public IEnumerable<OrderItem> GetList(Func<OrderItem, bool>? condition)
